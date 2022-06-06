7d3438c375995023c236d96fd516f10d58c5dfc6
jagodabie <51316252+jagodabie@users.noreply.github.com>
2022-06-06 12:04:58 +0200

    feat: baseinput emit implementation, styleing login page

--- ecommerce/src/components/BaseInput.vue
+++ ecommerce/src/components/BaseInput.vue
@@ -1,5 +1,7 @@
 <script setup lang="ts">
-import { computed } from "vue";
+import { computed, watchEffect, ref, defineEmits } from "vue";
+import type { Ref } from "vue";
+
 // TODO props
 const props = defineProps({
   label: String,
@@ -10,26 +12,56 @@ const props = defineProps({
   maxLength: Number,
   modelValue: String,
 });
+const emit = defineEmits<{
+  (e: "update:modelValue", value: string | number | null): void;
+}>();
+
+const modelValue = computed({
+  get() {
+    return props.modelValue;
+  },
+  set(value) {
+    return emit("update:modelValue", value as string | number | null);
+  },
+});
 </script>
 
 <template>
   <label v-if="props.label" id="base-input-label-id-test">
-    {{ props.modelValue }}</label
+    {{ props.label }}</label
   >
   <input
     id="base-input-id-test"
     v-bind="$attrs"
-    :type="props.type"
+    v-model="modelValue"
+    :type="props.type ?? 'text'"
     :name="props.name"
     :placeholder="props.placeholder"
-    :value="props.modelValue"
+    :minlength="props.minLength"
+    :maxlength="props.maxLength"
   />
 </template>
 <style scoped>
 input {
   width: 100%;
-  padding: 12px 20px;
-  margin: 8px 0;
+  padding: 1.1rem 1.1rem;
+  margin: 1.2rem 0;
   box-sizing: border-box;
+  border: solid 2px rgb(99, 99, 105);
+  border-radius: 7px;
+  font-size: 1rem;
+}
+
+label {
+  padding: 0.5rem 0 0.5rem;
+  font-size: 1.2rem;
+  font-weight: bold;
+}
+
+input:focus {
+  border: none;
+  outline: 2px solid rgb(167, 160, 152);
+  box-shadow: 0 0 10px rgb(99, 99, 105);
+  border-radius: 4px;
 }
 </style>
